56556f6029eb9c2585a2a1a6399384eb94fe7121
YuriLohan <yurilohan1000@gmail.com>
2025-02-09 18:57:40 -0300

    Detona-Ralph-gamev1

---
+++ src/scripts/engine.js
@@ -0,0 +1,62 @@
+const state = {
+    view:{
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values:{
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions:{
+        timerId: setInterval(randomSquare, 500),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+    let randomNumber = Math.floor(Math.random()*9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square)=> {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                playSound("hit");
+                state.values.hitPosition = null;
+            }
+        })
+    })
+}
+function countDown(){
+    state.values.currentTime --;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if (state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId)
+        clearInterval(state.actions.timerId)
+        alert("Game Over, o resultado foi de "+state.values.result+" pontos.");
+        window.location.reload(true);
+    }
+}
+
+function playSound(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.1;
+    audio.play();
+}
+
+function main(){
+    addListenerHitBox();
+}
+main()
